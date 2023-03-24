021e3910ccf483abef9e336bf503b65feaf1cec9
Barbara <124930696+babirossi@users.noreply.github.com>
2023-03-24 17:09:31 -0300

    Add files via upload

---
+++ senaiLivre/admin.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Cadastro</title>
+</head>
+<body>
+    
+
+<div class="divtopo">
+
+        <div class="lateral">
+            <img src="img/senai-logo1.png" alt="">
+
+            <button><i class="fa-solid fa-table-cells-large"></i>Painel de controle</button>
+            <button><i class="fa-regular fa-address-book"></i>Usuários</button>
+            <button><i class="fa-solid fa-user-pen"></i>Minha conta</button>
+            <button><i class="fa-solid fa-user-large"></i>Segurança</button>
+            <button><i class="fa-regular fa-file"></i>Corporativo</button>
+            <button><i class="fa-solid fa-volleyball"></i>Esportes</button>
+            <button><i class="fa-solid fa-landmark"></i>Acadêmico</button>
+            <button><i class="fa-solid fa-chart-pie"></i>Relatórios</button>
+            <button><i class="fa-solid fa-fill-drip"></i>Custumozação</button>
+            <button><i class="fa-solid fa-land-mine-on"></i>Alertas</button>
+        </div>
+
+        <div class="conteudo">
+
+            <header class="topo">
+                <nav>
+                    <a href="">Dashboards</a>
+                    <a href="">Páginas</a>
+                    <a href="">Aplicativos</a>
+                    <a href="">Ajuda</a>
+                </nav>
+
+                <form action="" method="POST">
+                    <input type="text">
+                    <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </form>
+            </header>
+
+            <main class="corpo">
+
+                <div class="painelcontrole">
+                    <h1>E-commerce | Painel de Controle</h1>
+
+                    <div class="vendasdomes">
+                        <p>aqui vai ser o valor</p>
+                        <p>Vendas esse mês</p>
+                        <i class="fa-solid fa-coins"></i>
+                    </div>
+
+                    <div class="produtosativos">
+                        <p>345</p>
+                        <p>Produtos ativos</p>
+                        <i class="fa-brands fa-dropbox"></i>
+                    </div>
+
+                    <div class="fornecedores">
+                        <p>25</p>
+                        <p>Fornecedores</p>
+                        <i class="fa-regular fa-building"></i>
+                    </div>
+
+                    <div class="clientes">
+                        <p>4500</p>
+                        <p>Clientes</p>
+                        <i class="fa-regular fa-user"></i>
+                    </div>
+
+                </div>
+
+                <div class="carrinho">
+                    <div class="carrinhoativo">
+                        <h4>Carrinhos ativos</h4>
+                        <button><i class="fa-solid fa-shirt"></i>Moda</button>
+                        <button><i class="fa-solid fa-gamepad"></i>Jogos</button>
+                        <button><i class="fa-solid fa-clock"></i>Eletrônicos</button>
+                        <button><i class="fa-solid fa-dumbbell"></i>Esporte</button>
+                        <button><i class="fa-solid fa-shoe-prints"></i>Tênis</button>
+
+                        <table>
+                            <tr>
+                                <td>Item</td>
+                                <td>QTY</td>
+                                <td>Preço</td>
+                                <td>Valor total</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div>
+                                        <img src="img/blusaelef.png" alt="">
+                                        <p>Elephant 1802</p>
+                                        <p>Item: #XDG-2347</p>
+                                       
+                                    </div>
+                                    <div>
+                                        <img src="img/blusared.png" alt="">
+                                        <p>Red Laga</p>
+                                        <p>Item:#XDG-1321</p>
+                                    </div>
+                               
+                                    <div>
+                                        <img src="img/blusadark.png" alt="">
+                                        <p>RiseUP</p>
+                                        <p>Item:#XDG-4312</p>
+                                       
+                                    </div>
+                                </td>
+                                <td>
+                                    <div>
+                                        <p>X1</p>
+                                    </div>
+                                    <div>
+                                        <p>X2</p>
+                                    </div>
+                               
+                                    <div>
+                                        <p>X3</p>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div>
+                                        <p>$72.00</p>
+                                    </div>
+                                    <div>
+                                        <p>$45.00</p>
+                                    </div>
+                               
+                                    <div>
+                                        <p>$84.00</p>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div>
+                                        <p>$126.00</p>
+                                    </div>
+                                    <div>
+                                        <p>$76.00</p>
+                                    </div>
+                               
+                                    <div>
+                                        <p>$168.00</p>
+                                    </div>
+                                </td>
+                            </tr>    
+
+                        </table>
+                    </div>
+
+                    <div class="novoaplicativo">
+                        <h3>Novo aplicativo</h3>
+                        <h3>de E-commerce</h3>
+                        <img src="img/pessoa.PNG" alt="">
+                        <br>
+                        <button>Ver Novidades</button>
+                        <button>Ver Produtos</button>
+                    </div>
+                </div>
+               
+                <div class="pedidovendas">
+                    <div class="pedido">
+                        <p>Pedidos</p>
+                        <p>No último mês</p>
+                        <button>Mais detalhes</button>
+
+                        <div>
+                            <img src="img/camisetaEle.png" alt="">
+                            <p>Elephant 1802</p>
+                            <p>To: Jason Bourne</p>
+
+                            <div>
+                                <button><i class="fa-solid fa-ellipsis"></i></button>
+                                <p>Pago</p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <img src="img/camisetapreta.png" alt="">
+                            <p>RiseUP</p>
+                            <p>To: Marie Durant</p>
+
+                            <div>
+                                <button><i class="fa-solid fa-ellipsis"></i></button>
+                                <p>Processando</p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <img src="img/camisetamarela.png" alt="">
+                            <p>Yellow Stone</p>
+                            <p>To: Dan Wilson</p>
+
+                            <div>
+                                <button><i class="fa-solid fa-ellipsis"></i></button>
+                                <p>Cancelado</p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <img src="img/camisetaesporte.png" alt="">
+                            <p>Elephant 1802</p>
+
+                            <div>
+                                <button><i class="fa-solid fa-ellipsis"></i></button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="venda">
+                        <table>
+                            <tr>
+                                <td>ITEM</td>
+                                <td>PRODUTOS</td>
+                                <td>DATA</td>
+                                <td>PREÇO</td>
+                                <td>STATUS</td>
+                                <td>QTY</td>
+                            </tr>
+                            <tr>
+                                <td>Macbook Air M1</td>
+                                <td>#XGY-356</td>
+                                <td>02 Apr, 2023©</td>
+                                <td>R$ 100</td>
+                                <td>In Stock</td>
+                                <td>58 Pcs</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+
+            </main>
+
+            <footer class="rodape">
+                <p>2023© Senai Livre - Sistema de Controle</p>
+            </footer>
+        </div>
+
+    </div>
+    <script src="https://kit.fontawesome.com/92de602b18.js" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ senaiLivre/login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>login</title>
+</head>
+<body>
+    <header id="logo">
+        <img src="img/senai.png" alt="logo">
+
+        <nav>
+            <a href="index.html">Sair</a>
+        </nav>
+    </header>
+
+    <main class="inscreva-se">
+        <section id="form">
+
+            <h1>Por favor, inscreva-se</h1>
+            
+            <div id="form-formulario">
+
+                <form action="" method="POST">
+
+                    <div class="form-formulario-nome">
+                        <label for="login">Login</label>
+                        <input type="text" name="login" id="login" placeholder="Informe seu login" required> 
+                    </div>
+
+                    <div class="form-formulario-senha">
+                        <label for="senha">Senha</label>
+                        <input type="password" name="senha" id="senha" placeholder="Informe sua senha" required> 
+                    </div>
+
+                    <div class="login">
+                        <button type="submit">Enviar</button>
+                    </div>
+                </form>
+
+                <div class="nao-cadastrado">
+                    <a href="cadastro.html"><button type="submit">Ainda não cadastrado? Cadastra-se</button></a>
+                </div>
+
+            </div>
+        </section>
+    </main>
+</body>
+</html>
+
+<?php
+    include("conexao.php");
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $login = $_POST["login"];
+        $senha = $_POST["senha"];
+
+        $sql = "SELECT * FROM usuario WHERE login = :login and senha = :senha";
+        $stmt = $conexao->prepare($sql);
+        $stmt->bindValue(":login", $login);
+        $stmt->bindValue(":senha", $senha);
+        $stmt->execute();
+
+        if($stmt->rowCount() > 0){
+
+        }else {
+            
+        }
+    }
+?>
